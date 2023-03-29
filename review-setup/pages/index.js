b66c6cf243847ebf2249dc1c6af1920d33fe2f88
AbdelrhmanEhab10 <anab7bmessi@gmail.com>
2023-03-29 16:39:49 +0200

    push projects

--- review-setup/pages/index.js
+++ review-setup/pages/index.js
@@ -1,10 +1,34 @@
 import Card from "@/components/Card";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card />
-    </div>
+    <>
+      <Head>
+        <title>Review Setup</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
+      <div className="flex justify-center items-center h-screen">
+        <Card />
+      </div>
+    </>
   );
 };
 
